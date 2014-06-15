3f57b78cd357bd740c39d7ef12d54ff44047c14b
Johnny <j796160836@gmail.com>
2014-06-15 10:41:47 +0800

    - Remove unused GlassVoice-xe12.jar - Add development permission at can support XE17 above devices - Remove RobotoTypefaces that lead to crash

--- imu_scrollview/src/de/tud/ess/ImuScrollView.java
+++ imu_scrollview/src/de/tud/ess/ImuScrollView.java
@@ -6,22 +6,18 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.TextView;
 
-import com.google.glass.widget.RobotoTypefaces;
-
 public class ImuScrollView extends Activity {
-  private TextView mTextView;
+	private TextView mTextView;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    
-    setContentView(R.layout.main);
-    mTextView       = (TextView) findViewById(R.id.textView);
-    
-    /* Typeface can't be set in layout specs prior to API16 */
-    Typeface roboto = RobotoTypefaces.getTypeface(this, RobotoTypefaces.WEIGHT_THIN);
-    mTextView.setTypeface(roboto);
-    mTextView.setText("\n\nHello World!\nHello World again!\nAnd again hello World!\n\n\n\nAnd some more at the bottom!\n\n");
-    mTextView.setGravity(Gravity.CENTER);
-  }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main);
+		mTextView = (TextView) findViewById(R.id.textView);
+
+		mTextView
+				.setText("\n\nHello World!\nHello World again!\nAnd again hello World!\n\n\n\nAnd some more at the bottom!\n\n");
+		mTextView.setGravity(Gravity.CENTER);
+	}
 }
